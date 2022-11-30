ee1b481fc210d938483308958e66b05266c3ff2c
dene009 <esidene009@gmail.com>
2022-11-30 10:23:10 -0800

    completed task

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Author - Esidene Alicha
+
+def islower(c):
+
+    """Function checks for lowercase characters."""
+
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
