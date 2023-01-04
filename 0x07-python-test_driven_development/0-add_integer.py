7f0f2e87b6bf0b801cc41e70063929d6188646a7
dene009 <esidene009@gmail.com>
2023-01-04 04:08:54 -0800

    tedr

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
 """Defines an integer addition function."""
-
 
 def add_integer(a, b=98):
 
@@ -12,11 +10,9 @@ def add_integer(a, b=98):
     """
 
     if ((not isinstance(a, int) and not isinstance(a, float))):
-
         raise TypeError("a must be an integer")
 
     if ((not isinstance(b, int) and not isinstance(b, float))):
-
         raise TypeError("b must be an integer")
 
     return (int(a) + int(b))
