d8e2ddfa0dc5e1294dac2a6945b36c904ad4c5ff
dene009 <esidene009@gmail.com>
2023-03-14 22:03:32 +0000

    100 map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
+/* Imports an array and computes new array */
+const list = require('./100-data').list;
 
-const newList = list.map((val, idx) => val * idx);
 console.log(list);
-console.log(newList);
+console.log(list.map((v, i) => v * i));
