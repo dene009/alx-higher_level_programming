a99b5f39d1f45ee2902044b2b6b5e1509f5517ac
dene009 <esidene009@gmail.com>
2023-03-14 22:33:36 +0000

    warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-
-/* Prints 3 lines by using an array of strings and a loop */
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-for (let i = 0; array[i]; ++i) {
-  console.log(array[i]);
+// Basic print with Javascript
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
